725fc460674b3865dc77634a89fc943c24f67977
John Hester <hesterj@etableau.com>
2026-05-26 09:03:15 -0500

    feat: remove python

--- gnu/packages/longleaf.scm
+++ gnu/packages/longleaf.scm
@@ -6,8 +6,6 @@
   #:use-module (guix git-download)
   #:use-module (guix gexp)
   #:use-module (guix build-system longleaf-dune)
-  #:use-module (guix build-system python)
-  #:use-module (guix build-system pyproject)
   #:use-module (guix build-system copy)
   #:use-module (guix build-system gnu)
   #:use-module (guix build-system cmake)
@@ -21,19 +19,6 @@
   #:use-module (gnu packages curl)
   #:use-module (gnu packages node)
   #:use-module (gnu packages maths)
-  #:use-module (gnu packages finance)
-  #:use-module (gnu packages libffi)
-  #:use-module (gnu packages python)
-  #:use-module (gnu packages python-xyz)
-  #:use-module (gnu packages python-science)
-  #:use-module (gnu packages python-web)
-  #:use-module (gnu packages python-build)
-  #:use-module (gnu packages time)
-  #:use-module (gnu packages xml)
-  #:use-module (gnu packages protobuf)
-  #:use-module (gnu packages statistics)
-  #:use-module (gnu packages python-crypto)
-  #:use-module (gnu packages databases)
   #:use-module (gnu packages emacs)
   #:use-module (gnu packages emacs-xyz)
   #:use-module (guix build-system emacs)
@@ -192,111 +177,8 @@ handling with Result types.")
  (license license:gpl3+)))
 
 
-;;; Python Packages
 
-;; multitasking - Non-blocking Python methods using decorators
-(define-public python-multitasking
-  (package
-   (name "python-multitasking")
-   (version "0.0.12")
-   (source
-    (origin
-     (method url-fetch)
-     (uri (string-append "https://files.pythonhosted.org/packages/source/m/multitasking/multitasking-"
-                         version ".tar.gz"))
-     (sha256
-      (base32 "1lc4kcs5fnhp2rrr4izjnviqsrbx3k27vpf54zi8ajwcxnl2zfig"))))
-   (build-system python-build-system)
-   (arguments
-    '(#:tests? #f))  ; No tests included in source
-   (home-page "https://github.com/ranaroussi/multitasking")
-   (synopsis "Non-blocking Python methods using decorators")
-   (description
-    "MultiTasking is a lightweight Python library that lets you convert your
-Python methods into asynchronous, non-blocking methods simply by using a
-decorator.  Perfect for I/O-bound tasks, API calls, web scraping, and any
-scenario where you want to run multiple operations concurrently without the
-complexity of manual thread or process management.")
-   (license license:asl2.0)))
 
-;; yfinance - Market data downloader
-;; NOTE: Pinned to v0.2.57 to avoid curl_cffi dependency introduced in v0.2.58+
-(define-public python-yfinance
-  (package
-   (name "python-yfinance")
-   (version "0.2.57")
-   (source
-    (origin
-     (method url-fetch)
-     (uri (string-append "https://files.pythonhosted.org/packages/source/y/yfinance/yfinance-"
-                         version ".tar.gz"))
-     (sha256
-      (base32 "1cgkch19a1rn175ixd8180a47dnc8nmwljyj2538s55ijyd385kv"))))
-   (build-system python-build-system)
-   (arguments
-    '(#:tests? #f     ; Skip tests that require network access
-      #:phases
-      (modify-phases %standard-phases
-                     (delete 'sanity-check))))  ; Skip sanity check that enforces curl_cffi dependency
-   (propagated-inputs
-    (list python-pandas
-          python-numpy
-          python-requests
-          python-lxml
-          python-appdirs
-	  python-platformdirs
-          python-pytz
-          python-beautifulsoup4
-          python-websockets
-          python-protobuf
-          python-frozendict
-          python-peewee))
-   (home-page "https://github.com/ranaroussi/yfinance")
-   (synopsis "Download market data from Yahoo! Finance API")
-   (description
-    "yfinance is a Python library that offers a threaded and Pythonic way
-to download market data from Yahoo! Finance.  It fixes the temporary
-authentication and decryption issues by dynamically scraping the data.")
-   (license license:asl2.0)))
-
-;; quantstats - Portfolio analytics for quants
-(define-public python-quantstats
-  (package
-   (name "python-quantstats")
-   (version "0.0.75")
-   (source
-    (origin
-     (method url-fetch)
-     (uri (string-append "https://github.com/ranaroussi/quantstats/archive/"
-                         version ".tar.gz"))
-     (file-name (string-append name "-" version ".tar.gz"))
-     (sha256
-      (base32 "04a8r3rr36asij0dw31crvzj15xzz972drk48f9a80py0ha7431s"))))
-   (build-system python-build-system)
-   (arguments
-    '(#:tests? #f     ; Skip tests that require yfinance network access
-      #:phases
-      (modify-phases %standard-phases
-                     (delete 'sanity-check))))  ; Skip sanity check that enforces yfinance curl_cffi dependency
-   (propagated-inputs
-    (list python-pandas
-          python-numpy
-          python-scipy
-          python-matplotlib
-          python-seaborn
-          python-tabulate
-          python-dateutil
-          python-packaging
-          python-yfinance
-          python-multitasking))
-   (home-page "https://github.com/ranaroussi/quantstats")
-   (synopsis "Portfolio analytics for quants")
-   (description
-    "QuantStats is a Python library that performs portfolio analytics for quants.
-It provides in-depth analytics and risk metrics for quantitative analysts
-and portfolio managers including Sharpe ratio, win rate, volatility, drawdowns,
-rolling statistics, monthly returns, and various performance tear sheets.")
-   (license license:asl2.0)))
 
 ;; Full Longleaf package with all dependencies
 (define-public longleaf
@@ -319,13 +201,8 @@ rolling statistics, monthly returns, and various performance tear sheets.")
   ;; No tests.
   '(#:tests? #f))
  (inputs (list
-          python-quantstats
-          python-yfinance
-          python-multitasking
           nlopt
-          node
-          )
-         )
+          node))
  (propagated-inputs
   (list
    ocaml-ptime
